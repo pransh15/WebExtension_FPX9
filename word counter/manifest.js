f54ec78299cdb8b826cbdaf146a29e1c8010a436
Nisarg-Joshi <47933607+Nisarg-Joshi@users.noreply.github.com>
2019-02-24 09:48:25 +0530

    word counter extension Nisarg
    Count the number of words and letters of selected text

---
+++ word counter/manifest.js
@@ -0,0 +1,12 @@
+{
+	"manifest_version":2,
+	"name":"Word Count",
+	"version":"1.0",
+	"description": "Count words of selected text",
+	"icons":{
+		"48":"icon.png"
+	},
+"background":[{
+	   "scripts":["bg.js"]
+}]
+}
